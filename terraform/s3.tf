8101312be6848bcc3af962a13e48fd3283a7650b
Manuel Padial <manolo.padial.2001@gmail.com>
2025-05-04 21:28:51 +0200

    no se que esta asando

--- terraform/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = "scraping-lambda-bucket-auto"  # MISMO NOMBRE que en el YAML
-  force_destroy = true
-
-  tags = {
-    Name        = "LambdaCodeBucket"
-    Environment = "dev"
-  }
-}
